75c7218dd705e58d3b93f384089dcb83ad166797
jcvic1 <victorjcdon@gmail.com>
2024-12-18 04:13:39 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { SpeedInsights } from '@vercel/speed-insights/react';
+import { Analytics } from '@vercel/analytics/react';
 import Homepage from "./app/pages/homepage";
 import ThemeContext from "./app/contexts/theme";
 
@@ -6,9 +8,13 @@ import ThemeContext from "./app/contexts/theme";
 const App = () => {
   const [theme, setTheme] = useState("dark");
   return (
+    <>
     <ThemeContext.Provider value={{ theme, setTheme }}>
       <Homepage />
     </ThemeContext.Provider>
+    <SpeedInsights />
+    <Analytics />
+    </>
   );
 };
 
